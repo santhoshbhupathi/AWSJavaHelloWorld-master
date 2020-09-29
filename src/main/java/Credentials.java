f60c69c51d414f3991dd6a121010e151ee6ebd57
santhosh <bhupathisanthosh@gmail.com>
2020-09-29 21:38:35 +0530

    intial
    first

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mattua on 23/07/2016.
+ */
+public class Credentials {
+
+
+    public static final String access_key_id= "AKIAJQBA6TTUVSE7ROHA";
+
+    public static final String secret_access_key ="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";
+
+
+
+}
+
